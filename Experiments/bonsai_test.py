7d05d7a0e2b69b8a177f5083b0ba068fd31069a6
bonsai-tree-model <bonsai.tree.model@gmail.com>
2025-05-14 13:41:13 +0900

    Revised

---
+++ Experiments/bonsai_test.py
@@ -0,0 +1,25 @@
+import importlib
+import bonsai_ml
+
+if __name__ == "__main__":
+    df_pq = pd.read_csv('dataset/penguins/penguins_train.csv')
+    df_pq_test = pd.read_csv('dataset/penguins/penguins_test.csv')
+    attributes = {}
+    attributes['Decision'] = 'Output'
+    attributes['island'] = 'Categorical'
+    attributes['culmen_length_mm'] = 'Numerical'
+    attributes['culmen_depth_mm'] = 'Numerical'
+    attributes['flipper_length_mm'] = 'Numerical'
+    attributes['body_mass_g'] = 'Numerical'
+    attributes['sex'] = 'Categorical'
+    attributes['culmen_ratio'] = 'Numerical' # Extracted Feature
+
+    max_nodes = 20
+    point = list(range(1,3))
+    df_pq_copy = copy.deepcopy(df_pq)
+    bonsai_ml.RANGE_TOLERANCE = 1.0
+    bonsai_ml.SAMPLE_RATIO = 0.001
+    importlib.invalidate_caches()
+    lazy_rule = lazy_import('rule')
+    accuracy, tree = bonsai_ml.train(df_pq_copy, attributes, rule_path='rule', tree_path='nodes.tree', min_sample=2)
+    save_tree(tree, 'rule_test')
